6ff1e65a3603a61d98b0755565ac03363640dae2
Nixon O. Marinho <nixon.devsistemas@gmail.com>
2025-11-05 20:03:16 -0400

    Iniciando o site de vendas de inglês

---
+++ js/meuJS.js
@@ -0,0 +1,8 @@
+// Tempo em milissegundos (3min 43s = 1*30*1000*1000)
+  const delay = 60 * 1000; // 1 min
+
+  // Após o tempo, exibe a section
+  setTimeout(() => {
+    document.getElementById("minhaSection").style.display = "block";
+    document.getElementById("minhaNave").style.display = "block";
+  }, delay);
